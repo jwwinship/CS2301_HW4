7c87b5cb6abde39cdac60d2d88a1fbff4ff4e7db
BuildTools <cubemaster118@gmail.com>
2020-12-09 00:23:54 -0500

    NOW All the stubs are there

--- src/AdjMat.c
+++ src/AdjMat.c
@@ -7,31 +7,15 @@
 
 void init(AdjMat* adjMP)
 {
-    int ncols = adjMP->n;
-    printf("In init with ncols = %d\n", ncols);
-    for(int row = 0; row<ncols; row++)
-    {
-        for(int col = 0; col<ncols; col++)
-        {
-            int* memoryToAssign = ((adjMP->edgesP)+(row*ncols)+col);
-            *memoryToAssign= 0;
-        }
-    }
 
 }
 
 void setEdge(AdjMat* adjMP, int row, int col)
 {
 
-    int ncols = adjMP->n;
-    int* arrayBeginning = adjMP->edgesP;
-    *(arrayBeginning + (ncols*row) +col) = 1;
-    *(arrayBeginning + (ncols*col) + row) = 1;
 }
 int getEdge(AdjMat* adjMP, int row, int col)
 {
-    int ncols = adjMP->n;
-    int* arrayBeginning = adjMP->edgesP;
-    int returnValue = *(arrayBeginning + (ncols*row) +col); //Wrong value for test.
-    return returnValue;
+
+    return 0;
 }
